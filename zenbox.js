bb22036ad5411314afac995b6d50f07e8da802f5
Mike Schore <mike.schore@gmail.com>
2012-03-19 03:50:21 -0700

    v0.1.0

--- zenbox.js
+++ zenbox.js
@@ -1,3 +1,22 @@
+/* =========================================================
+ * zenbox v0.1.0
+ * https://github.com/goaway/zenbox
+ * =========================================================
+ * Copyright 2012 Michael Schore
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
 !function($) {
 
   "use strict";
@@ -54,7 +73,9 @@
         height: target[0].offsetHeight,
         width:  target[0].offsetWidth
       });
-      elements.removeClass("staging");
+      setTimeout(function() { /* hack to get staging styles to render in opera */
+        elements.removeClass("staging");
+      }, 50);
     }
   ;
 
